7eda848b3d1162c823b4aecb7d83fb39759b7d1a
sobakavosne <sobakavosne@protonmail.com>
2023-04-18 05:34:11 +0400

    add Week02 homework

--- code/Week02/homework/Homework1.hs
+++ code/Week02/homework/Homework1.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
 module Homework1 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
 import           PlutusTx             (compile)
-import           PlutusTx.Prelude     (Bool (..), BuiltinData)
+import           PlutusTx.Prelude     (Bool, BuiltinData, traceIfFalse, ($),
+                                       (&&))
 import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -16,7 +18,7 @@ import           Utilities            (wrapValidator)
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are True!
 mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
-mkValidator _ _ _ = False
+mkValidator _ (x1, x2) _ = traceIfFalse "Both of the Booleans should be True" $ x1 && x2
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 wrappedVal = wrapValidator mkValidator
--- code/Week02/homework/Homework2.hs
+++ code/Week02/homework/Homework2.hs
@@ -3,15 +3,16 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
 module Homework2 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
-import           PlutusTx             (unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool, BuiltinData)
-import           Prelude              (undefined)
---import           Utilities            (wrapValidator)
+import           PlutusTx             (compile, unstableMakeIsData)
+import           PlutusTx.Prelude     (Bool, BuiltinData, traceIfFalse, ($),
+                                       (/=))
+import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -26,10 +27,11 @@ PlutusTx.unstableMakeIsData ''MyRedeemer
 {-# INLINABLE mkValidator #-}
 -- Create a validator that unlocks the funds if MyRedemeer's flags are different
 mkValidator :: () -> MyRedeemer -> PlutusV2.ScriptContext -> Bool
-mkValidator = undefined
+mkValidator _ r _ = traceIfFalse "Expected different flags" $ flag1 r /= flag2 r
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-wrappedVal = undefined
+wrappedVal = wrapValidator mkValidator
+{-# INLINABLE wrappedVal #-}
 
 validator :: PlutusV2.Validator
-validator = undefined
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
